834fafbab7dd0f45fb57c9bf3eceda8c6c4b0637
SUPERBOLIDEIRL <122998649+SUPERBOLIDEIRL@users.noreply.github.com>
2024-03-17 02:29:39 -0400

    Add files via upload

---
+++ csharp-client/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using WebSocketSharp;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        private WebSocket ws;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeWebSocket();
+        }
+
+        private void InitializeWebSocket()
+        {
+            ws = new WebSocket("ws://127.0.0.1:7890/EchoAll");
+            ws.OnOpen += (sender, e) =>
+            {
+                Console.WriteLine("WebSocket connection established");
+            };
+            ws.OnMessage += (sender, e) =>
+            {
+                Console.WriteLine("Message received: " + e.Data);
+            };
+            ws.Connect();
+        }
+
+        private void InkCanvas_StrokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e)
+        {
+            var strokesData = new List<Point>();
+            foreach (var stroke in inkCanvas.Strokes)
+            {
+                foreach (var stylusPoint in stroke.StylusPoints)
+                {
+                    strokesData.Add(new Point(stylusPoint.X, stylusPoint.Y));
+                }
+            }
+
+            byte[] packetBytes = SerializePacket(strokesData);
+            ws.Send(packetBytes);
+        }
+
+        private byte[] SerializePacket(List<Point> coordinates)
+        {
+            var packetTypeBytes = BitConverter.GetBytes((int)PacketType.DrawingData);
+            var packetLengthBytes = BitConverter.GetBytes(coordinates.Count * 2 * sizeof(double));
+
+            var packetDataBytes = new List<byte>();
+            foreach (var point in coordinates)
+            {
+                packetDataBytes.AddRange(BitConverter.GetBytes(point.X));
+                packetDataBytes.AddRange(BitConverter.GetBytes(point.Y));
+            }
+
+            var packetBytes = new List<byte>();
+            packetBytes.AddRange(packetTypeBytes);
+            packetBytes.AddRange(packetLengthBytes);
+            packetBytes.AddRange(packetDataBytes);
+
+            return packetBytes.ToArray();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ws.Close();
+        }
+
+        public enum PacketType
+        {
+            DrawingData
+        }
+
+        public struct Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+
+            public Point(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
---
+++ csharp-server/csharp-server/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WpfApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer(IPAddress.Parse("127.0.0.1"), 7890);
+            wssv.AddWebSocketService<Echo>("/EchoAll");
+            wssv.Start();
+
+            Console.WriteLine("WebSocket server started at ws://127.0.0.1:7890/EchoAll");
+
+            Console.ReadKey();
+            wssv.Stop();
+        }
+
+        public class Echo : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                try
+                {
+                    var packet = DrawingPacket.Deserialize(e.RawData);
+                    Console.WriteLine($"Received {packet.Type} packet with {packet.Coordinates.Count} coordinates");
+
+                    // Broadcast the received drawing data to all clients
+                    Sessions.Broadcast(e.RawData);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error handling packet: {ex.Message}");
+                }
+            }
+        }
+    }
+
+    public struct DrawingPacket
+    {
+        public PacketType Type { get; set; }
+        public int Length { get; set; }
+        public List<Point> Coordinates { get; set; }
+
+        public static DrawingPacket Deserialize(byte[] rawData)
+        {
+            DrawingPacket packet = new DrawingPacket();
+            packet.Type = (PacketType)BitConverter.ToInt32(rawData, 0);
+            packet.Length = BitConverter.ToInt32(rawData, 4);
+
+            packet.Coordinates = new List<Point>();
+            for (int i = 8; i < rawData.Length; i += 16)
+            {
+                double x = BitConverter.ToDouble(rawData, i);
+                double y = BitConverter.ToDouble(rawData, i + 8);
+                packet.Coordinates.Add(new Point(x, y));
+            }
+
+            return packet;
+        }
+    }
+
+    public enum PacketType
+    {
+        DrawingData
+    }
+
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
